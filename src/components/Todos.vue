bdc0d04127e11a71a716690e1b01b0d9820300a2
IbrahimG88 <goharyibrahim@gmail.com>
2020-04-01 10:08:40 +0200

    delete functionality completed

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,41 +1,56 @@
 <template>
   <div>
     <h3>Todos</h3>
+
     <div v-for="todo in allTodos" v-bind:key="todo.id" class="todo">{{ todo.title }}</div>
     <ul>
-      
-    <li  v-for="(item, index) in allInventory" v-bind:key="item.name" class="inventoryItem">
-      <div v-if="showAllItems">
-      {{item}}
-      
-      <div v-if="showItem">
-      {{ item.name }} 
-      </div>
-      </div>
+      <li v-for="(item, index) in allInventory" v-bind:key="item.name" class="inventoryItem">
+        <div v-if="showAllItems">
+          {{item}}
+          <i @click="deleteItem(item.id)" class="fas fa-trash-alt"></i>
+          <div v-if="showItem">{{ item.name }}</div>
+        </div>
         <button @click="editItem(item, index)">Edit</button>
       </li>
     </ul>
     <div v-if="editing">
       {{index}}
       <!-- we will use the index from data() as given value in editItem(): this.index= index; -->
-        <input  type="text" v-model.lazy="allInventory[index].name" >
-        <button @click="editItem(item, index)">Edit finished</button>
+      <input type="text" v-model.lazy="allInventory[index].name" />
+      <button @click="editItem(item, index)">Edit finished</button>
+    </div>
+    <autocomplete
+      :search="search"
+      placeholder="Search for an Item"
+      aria-label="Search for an Item"
+      :get-result-value="getResultValue"
+    ></autocomplete>
+    <div>
+    <span v-if="showSearch">
+    {{ searchedItem }}
+    </span>
+   <span><i v-if="showSearch" @click="showSearch = !showSearch;" class="fas fa-trash-alt"></i> </span> 
     </div>
   </div>
 </template>
 
 <script>
-
 import { mapGetters, mapActions } from "vuex";
+import Autocomplete from "@trevoreyre/autocomplete-vue";
 
 export default {
   name: "Todos",
+  components: {
+    Autocomplete
+  },
   data() {
     return {
       showAllItems: true,
       editing: false,
       showItem: true,
-      index:null,
+      index: null,
+      showSearch: true,
+      searchedItem: '',
       item: {
         id: "",
         title: "",
@@ -48,14 +63,14 @@ export default {
           return this.testsPerUnit / this.testsUsedPerDay;
         }
       }
-    }
-    },
+    };
+  },
   methods: {
-    ...mapActions(["updateItem"]),
+    ...mapActions(["updateItem", "deleteItem"]),
     editItem(item, index) {
-      console.log("index",index);
+      console.log("index", index);
       console.log("item to edit all inventry:", this.allInventory[index]);
-      
+
       // theconditional rendering:
       this.showAllItems = !this.showAllItems;
       this.editing = !this.editing;
@@ -64,11 +79,28 @@ export default {
       //index from data() = item index clicked:
       // we use updateItem and passing the item that modifies allInventory[index]
       // then allInventory is updated => in the template: v-for=(item, index) in allInventory uses
-      // the updated, edited list 
-      this.index= index;
+      // the updated, edited list
+      this.index = index;
 
       this.updateItem(item);
       console.log(this.allInventory);
+    },
+    search(input) {
+      if (input.length < 1) {
+        return [];
+      }
+     
+      this.showSearch = true;
+
+      return this.allInventory.filter(item => {
+        if (item.name.toLowerCase().startsWith(input.toLowerCase())) {
+         
+          return item.name.toLowerCase().startsWith(input.toLowerCase());
+        }
+      });
+    },
+    getResultValue(result) {
+      this.searchedItem = result;
     }
   },
   computed: mapGetters(["allTodos", "allInventory"])
