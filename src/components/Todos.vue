6b349c401135a6f6c66d2f8f69062929b15b55cb
IbrahimG88 <goharyibrahim@gmail.com>
2020-04-11 00:41:57 +0200

    mounted () functions to apply daystilldepletion changes when the component mounts works

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -37,6 +37,7 @@
       <button
         @click="calculateDaysTillDepletion"
       >calculate days till depletion</button>
+     
     </div>
   </div>
 </template>
@@ -57,7 +58,7 @@ export default {
       //fpr luxon:
       //lastSavedTimets: ,
       //lastSavedTimedt:,
-      duration: 0,
+      duration:0,
       //
       objectItem: {},
       // the search component data ends here
@@ -115,77 +116,94 @@ export default {
       this.objectItem = this.options[0];
     },
     async calculateTimeDifference() {
-
-  
-/*
-      let lastSavedTimets = await db.collection("appVariables").doc("BBCqqInTGJw49nJ8YgSI").get().then(function(doc){
+      /* let lastSavedTimets = await db.collection("appVariables").doc("BBCqqInTGJw49nJ8YgSI").get().then(function(doc){
        //working now
-        
         //gets the value inside the object
         console.log('doc.data()',doc.data().lastSavedTimets);
         return doc.data().lastSavedTimets;
       });
       */
-
       //new change:
-      let lastSavedTimets = await db.collection("appVariables").doc("SavedTimeStamp").get().then(function(doc){
-       //working now
-        
-        //gets the value inside the object
-        console.log('doc.data()',doc.data().lastSavedTimets);
-        return doc.data().lastSavedTimets;
-      });
+      let lastSavedTimets = await db
+        .collection("appVariables")
+        .doc("SavedTimeStamp")
+        .get()
+        .then(function(doc) {
+          //working now
+          //gets the value inside the object
+          console.log("doc.data()", doc.data().lastSavedTimets);
+          return doc.data().lastSavedTimets;
+        });
 
+      console.log("ts", lastSavedTimets);
 
-     console.log("ts",lastSavedTimets);
-
-      console.log('firebase object',lastSavedTimets);
+      console.log("firebase object", lastSavedTimets);
       let lastSavedTimedt = DateTime.fromMillis(lastSavedTimets);
       //let now = DateTime.local();
       //timestamp ts
       // for now
       let ts = new Date().getTime(); //timestamp 3434324 for firebase
-      let dt = DateTime.fromMillis(ts);//datetime to be used in interval 
-
+      let dt = DateTime.fromMillis(ts); //datetime to be used in interval
 
       console.log("ts", ts);
-        console.log("dt", dt);
+      console.log("dt", dt);
       let i = Interval.fromDateTimes(lastSavedTimedt, dt);
-      let duration = i.toDuration("seconds").toObject();
-      let seconds = JSON.stringify(duration.seconds);
+      let iDuration = i.toDuration("seconds").toObject();
+      let seconds = JSON.stringify(iDuration.seconds);
       this.duration = seconds;
+      //good till here
+      console.log('duration',this.duration);
+     
+      lastSavedTimets = new Date().getTime(); //timestamp 23993294
+      lastSavedTimedt = DateTime.fromMillis(lastSavedTimets); // datetime
 
-      lastSavedTimets = new Date().getTime();//timestamp 23993294
-      lastSavedTimedt =  DateTime.fromMillis(lastSavedTimets); // datetime
-      
       //from the data()
-   // this.lastSavedTimets = lastSavedTimets;
-    //  this.lastSavedTimedt = lastSavedTimedt;
-/*
+      // this.lastSavedTimets = lastSavedTimets;
+      //  this.lastSavedTimedt = lastSavedTimedt;
+      /*
       await db.collection("appVariables").add({
         lastSavedTimets: lastSavedTimets
       });
       console.log(seconds);
 */
-    await db.collection("appVariables").doc("SavedTimeStamp").set({
-    lastSavedTimets: lastSavedTimets
-    });
+      await db
+        .collection("appVariables")
+        .doc("SavedTimeStamp")
+        .set({
+          lastSavedTimets: lastSavedTimets
+        });
 
+      return this.duration;
+       
     },
-    calculateDaysTillDepletion() {
-      let timePassed = this.duration;
-      console.log("time passed:" + timePassed);
+     calculateDaysTillDepletion(duration) {
+      //updates the store vuex
+      //let timePassed = this.duration;
+      //console.log("time passed:" + timePassed);
+      //this.duration is 0 from next line
+      console.log('this.duration',duration);
       this.allInventory.forEach(item => {
-        item.daysTillDepletion = (item.daysTillDepletion - timePassed).toFixed(
+        item.daysTillDepletion = (item.daysTillDepletion - duration).toFixed(
           1
         );
-
+        this.updateItem(item);
         return item;
       });
-    }
+
+      console.log('allinv', this.allInventory);
+      return this.allInventory;
+    },
+   
   },
   mounted() {
     console.log(this.lastSavedTime);
+    // if I try to add calculating functions they don't update when mounting the component
+   this.calculateTimeDifference().then((result)=>{
+     //returned is the duration
+     console.log('result: ',result);
+     this.calculateDaysTillDepletion(result);
+   });
+
   },
   computed: mapGetters(["allTodos", "allInventory"])
 };
